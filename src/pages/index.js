bb32d9b96e0df11158dff67cfe8364cdb0038ff8
MarlonMascarenhas <mmascarenhs@gmail.com>
2020-05-25 10:29:26 -0300

    borderRadius fixa

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './styles.css';
+
+function App() {
+  return (
+    <div className="longBar">
+      <div className= "borderMax">
+        <div className="borderRad" style = {{borderRadius: '15px 50px 30px 5px'}}>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
